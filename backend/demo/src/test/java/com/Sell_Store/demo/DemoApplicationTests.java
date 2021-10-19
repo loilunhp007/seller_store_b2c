6535ed30cabc41b9176c2ca97a6239ad15b8be1f
loilunhp007 <63707576+loilunhp007@users.noreply.github.com>
2021-10-19 13:34:24 +0700

    create project

---
+++ backend/demo/src/test/java/com/Sell_Store/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sell_Store.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
