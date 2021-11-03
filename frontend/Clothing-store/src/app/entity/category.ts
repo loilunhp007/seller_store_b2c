2148029d77f2ab6ebbd09c3f9c59dc856a8fa39c
loilunhp007 <63707576+loilunhp007@users.noreply.github.com>
2021-11-03 18:24:35 +0700

    fix entity id not auto increament

---
+++ frontend/Clothing-store/src/app/entity/category.ts
@@ -0,0 +1,6 @@
+export class Category {
+    cateid: number
+    tenloai:String
+    trangthai:number
+    
+}
---
+++ frontend/Clothing-store/src/app/entity/product.ts
@@ -0,0 +1,13 @@
+import { Category } from "./category"
+
+export class Product {
+    productid:string
+    name:string
+    cate:Category
+    productName:String
+    images:String
+    info:String
+    likes:number
+    views:number
+    state:number
+}
