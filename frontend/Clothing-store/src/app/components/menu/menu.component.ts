2e33ce8bbf410b1b7874bc31b7f4f8e390c46825
loilunhp007 <63707576+loilunhp007@users.noreply.github.com>
2021-11-11 20:04:04 +0700

    fix home+menu component

--- frontend/Clothing-store/src/app/components/menu/menu.component.ts
+++ frontend/Clothing-store/src/app/components/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/entity/cart';
 import { Category } from 'src/app/entity/category';
+import { CartService } from 'src/app/services/cart.service';
 import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
@@ -9,16 +11,31 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class MenuComponent implements OnInit {
   categories:Array<Category>
-  constructor(private categoryService:CategoryService) { }
+  carts:Array<Cart>
+  cartLength:number
+  constructor(private categoryService:CategoryService,
+    private cartService:CartService) { }
 
   ngOnInit(): void {
     this.getCategories();
+    this.getCart();
   }
   getCategories(){
     this.categoryService.getCategories().subscribe(Response=>{
       this.categories = Response;
       console.log(Response)
+    },(error)=>{
+      console.log(error)
+    });
+    
+  }
+  getCart(){
+    this.cartService.getCartItems(1+'').subscribe(Response=>{
+      this.carts=Response;
+      this.cartLength = this.carts.length;
+      console.log(this.carts);
+    },(error)=>{
+      console.log(error);
     })
   }
-
 }
