4365f6eb234b39410ac7a8407cf1831f91f5a25a
DucLuu050499 <luuanhduc19@gmail.com>
2021-11-16 16:34:36 +0700

    product

--- frontend/Clothing-store/src/app/components/product/product.component.ts
+++ frontend/Clothing-store/src/app/components/product/product.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/entity/product';
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  products:Array<Product>
+  productID:string;
+  product:Product;
+  constructor(
+    private productService:ProductService,
+    private Actroute: ActivatedRoute,
+    private router:Router,
+    ) { }
 
   ngOnInit(): void {
+    this.getAllProducts();
+    this.goDetail(this.product)
   }
-
+  getAllProducts(){
+    this.productService.getAllProducts().subscribe(Response =>{
+        this.products = Response;
+        console.log(this.products);
+    })
+  }
+  goDetail(product:Product){
+    this.Actroute.queryParams.subscribe( params  =>{
+      const id = product.productID;
+      this.router.navigate(['/','product-detail'],{queryParams:{id}})
+    })
+  }
 }
