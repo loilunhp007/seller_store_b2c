7b07d4d1236a2b718edcf2fe4d311746db6a3aa4
DucLuu050499 <luuanhduc19@gmail.com>
2021-11-18 22:23:53 +0700

    search

--- frontend/Clothing-store/src/app/components/search/search.component.ts
+++ frontend/Clothing-store/src/app/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/entity/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  s:String;
+  products: Array<Product>;
+  constructor(
+    private actroute: ActivatedRoute,
+    private productService: ProductService,
+  ) { }
 
   ngOnInit(): void {
+      this.getProductByLikeTensp(this.s);
+  }
+  getProductByLikeTensp(tensp:String){
+    this.productService.getProductByLikeTensp(tensp).subscribe(
+      Response =>{ this.products = Response}
+    )
   }
 
 }
