23411200796219d1e811f88446984c7799866447
DucLuu050499 <luuanhduc19@gmail.com>
2021-11-21 12:50:47 +0700

    components-admin

---
+++ frontend/Clothing-store/src/app/components-admin/phanquyen/phanquyen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phanquyen',
+  templateUrl: './phanquyen.component.html',
+  styleUrls: ['./phanquyen.component.css']
+})
+export class PhanquyenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
